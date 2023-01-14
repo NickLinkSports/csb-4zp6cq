2f88162de4684acd30a0e50f5e9b2a1a4232ac55
Code Sandbox <hello@codesandbox.io>
2023-01-14 14:01:35 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.c740b82329ec372df971b27a55d8c475.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719a13317beab890985af429d4cc6421",
+    "url": "https://nicklinksports.github.io/csb-4zp6cq/index.html"
+  },
+  {
+    "revision": "a944f8009d2f6bc0885c",
+    "url": "https://nicklinksports.github.io/csb-4zp6cq/static/js/2.43f751c5.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "https://nicklinksports.github.io/csb-4zp6cq/static/js/2.43f751c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea8a8799d2c5c0660acc",
+    "url": "https://nicklinksports.github.io/csb-4zp6cq/static/js/main.4e906573.chunk.js"
+  },
+  {
+    "revision": "78ea0e5321481f85cc00",
+    "url": "https://nicklinksports.github.io/csb-4zp6cq/static/js/runtime-main.a82898d2.js"
+  }
+]);
